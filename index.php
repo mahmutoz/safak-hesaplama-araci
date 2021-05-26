3fd47b5a7973bbfd0022a1103b21b79551ae96ae
mahmutoz <mahmutoz8197@gmail.com>
2021-05-26 21:25:32 +0300

    Options complated. Style updated

--- index.php
+++ index.php
@@ -11,51 +11,53 @@
     <title>Şafak Hesaplama</title>
 </head>
 <body>
-
     <div class="safak-container">
-        <form action="" method="get" class="safak-form">
+        <div class="error-message"><?php if($errorMsg != null) echo $errorMsg ?></div>
+        <h2 class="safak-title">ŞAFAK HESAPLAMA ARACI</h2>
+        <p class="safak-text"><span class="star">*</span> Doldurulması zorunlu alanlar.</p>
+        <form action="" method="post" class="safak-form">
             <div class="safak-wrapper">
                 <label>
-                    <p>Askerlik Süresi*</p>
-                    <select name="military-service-time" required>
-                        <option value="0">Seçiniz</option>
-                        <option value="30">1 Ay (Bedelli)</option>
-                        <option value="180">6 Ay (Er/Erbaş)</option>
-                        <option value="360">12 Ay (Yedek Subay/ Astsubay)</option>
+                    <p>Askerlik Süresi <span class="star">*</span></p>
+                    <select name="military-service-time">
+                        <option <?php echo post('military-service-time') == 0 ? ' selected' : null; ?> value="0">Seçiniz</option>
+                        <option <?php echo post('military-service-time') == 30 ? ' selected' : null; ?> value="30">1 Ay (Bedelli)</option>
+                        <option <?php echo post('military-service-time') == 180 ? ' selected' : null; ?> value="180">6 Ay (Er/Erbaş)</option>
+                        <option <?php echo post('military-service-time') == 360 ? ' selected' : null; ?> value="360">12 Ay (Yedek Subay/ Astsubay)</option>
                     </select>
                 </label>
                 <label>
-                    <p>Sevk Tarihi*</p>
-                    <input type="date" name="ship-date" min="<?php echo date('Y-m-d', strtotime('-365 day')) ?>" required>
+                    <p>Sevk Tarihi <span class="star">*</span></p>
+                    <input type="date" name="ship-date" value="<?php echo post('ship-date') ? post('ship-date') : null; ?>" min="<?php echo date('Y-m-d', strtotime('-365 day')) ?>" required>
                 </label>
                 <label>
                     <p>Kullanılan İzin</p>
                     <select name="permission-used">
-                        <option value="0">0 Gün (İzin kullanmadım)</option>
-                        <option value="1">1 Gün</option>
-                        <option value="2">2 Gün</option>
-                        <option value="3">3 Gün</option>
-                        <option value="4">4 Gün</option>
-                        <option value="5">5 Gün</option>
-                        <option value="6">6 Gün</option>
-                        <option value="7">7 Gün</option>
-                        <option value="8">8 Gün</option>
-                        <option value="9">9 Gün</option>
-                        <option value="10">10 Gün</option>
-                        <option value="11">11 Gün</option>
-                        <option value="12">12 Gün</option>
-                        <option value="13">13 Gün</option>
-                        <option value="14">14 Gün</option>
-                        <option value="15">15 Gün</option>
-                        <option value="16">16 Gün</option>
-                        <option value="17">17 Gün</option>
-                        <option value="18">18 Gün</option>
-                        <option value="19">19 Gün</option>
-                        <option value="20">20 Gün</option>
-                        <option value="21">21 Gün</option>
-                        <option value="22">22 Gün</option>
-                        <option value="23">23 Gün</option>
-                        <option value="24">24 Gün</option>
+                        <option <?php echo post('permission-used') == 0 ? ' selected' : null; ?> value="0">0 Gün (İzin kullanmadım)</option>
+                        <option <?php echo post('permission-used') == 1 ? ' selected' : null; ?> value="1">1 Gün</option>
+                        <option <?php echo post('permission-used') == 2 ? ' selected' : null; ?> value="2">2 Gün</option>
+                        <option <?php echo post('permission-used') == 3 ? ' selected' : null; ?> value="3">3 Gün</option>
+                        <option <?php echo post('permission-used') == 4 ? ' selected' : null; ?> value="4">4 Gün</option>
+                        <option <?php echo post('permission-used') == 5 ? ' selected' : null; ?> value="5">5 Gün</option>
+                        <option <?php echo post('permission-used') == 6 ? ' selected' : null; ?> value="6">6 Gün</option>
+                        <option <?php echo post('permission-used') == 7 ? ' selected' : null; ?> value="7">7 Gün</option>
+                        <option <?php echo post('permission-used') == 8 ? ' selected' : null; ?> value="8">8 Gün</option>
+                        <option <?php echo post('permission-used') == 9 ? ' selected' : null; ?> value="9">9 Gün</option>
+                        <option <?php echo post('permission-used') == 10 ? ' selected' : null; ?> value="10">10 Gün</option>
+                        <option <?php echo post('permission-used') == 11 ? ' selected' : null; ?> value="11">11 Gün</option>
+                        <option <?php echo post('permission-used') == 12 ? ' selected' : null; ?> value="12">12 Gün</option>
+                        <option <?php echo post('permission-used') == 13 ? ' selected' : null; ?> value="13">13 Gün</option>
+                        <option <?php echo post('permission-used') == 14 ? ' selected' : null; ?> value="14">14 Gün</option>
+                        <option <?php echo post('permission-used') == 15 ? ' selected' : null; ?> value="15">15 Gün</option>
+                        <option <?php echo post('permission-used') == 16 ? ' selected' : null; ?> value="16">16 Gün</option>
+                        <option <?php echo post('permission-used') == 17 ? ' selected' : null; ?> value="17">17 Gün</option>
+                        <option <?php echo post('permission-used') == 18 ? ' selected' : null; ?> value="18">18 Gün</option>
+                        <option <?php echo post('permission-used') == 19 ? ' selected' : null; ?> value="19">19 Gün</option>
+                        <option <?php echo post('permission-used') == 20 ? ' selected' : null; ?> value="20">20 Gün</option>
+                        <option <?php echo post('permission-used') == 21 ? ' selected' : null; ?> value="21">21 Gün</option>
+                        <option <?php echo post('permission-used') == 22 ? ' selected' : null; ?> value="22">22 Gün</option>
+                        <option <?php echo post('permission-used') == 23 ? ' selected' : null; ?> value="23">23 Gün</option>
+                        <option <?php echo post('permission-used') == 24 ? ' selected' : null; ?> value="24">24 Gün</option>
                     </select>
                 </label>
             </div>
@@ -63,54 +65,95 @@
                 <label>
                     <p>Yol İzin Hakkı</p>
                     <select name="road-permission">
-                        <option value="0">0 Gün</option>
-                        <option value="1">1 Gün</option>
-                        <option value="2">2 Gün</option>
-                        <option value="3">3 Gün</option>
-                        <option value="4">4 Gün</option>
+                        <option <?php echo post('road-permission') == 0 ? ' selected' : null; ?> value="0">0 Gün</option>
+                        <option <?php echo post('road-permission') == 1 ? ' selected' : null; ?> value="1">1 Gün</option>
+                        <option <?php echo post('road-permission') == 2 ? ' selected' : null; ?> value="2">2 Gün</option>
+                        <option <?php echo post('road-permission') == 3 ? ' selected' : null; ?> value="3">3 Gün</option>
+                        <option <?php echo post('road-permission') == 4 ? ' selected' : null; ?> value="4">4 Gün</option>
                     </select>
                 </label>
                 <label>
                     <p>TMİ Yol İzni</p>
                     <select name="tmi-road-permission">
-                        <option value="0">0 Gün</option>
-                        <option value="1">1 Gün</option>
-                        <option value="2">2 Gün</option>
+                        <option <?php echo post('tmi-road-permission') == 0 ? ' selected' : null; ?> value="0">0 Gün</option>
+                        <option <?php echo post('tmi-road-permission') == 1 ? ' selected' : null; ?> value="1">1 Gün</option>
+                        <option <?php echo post('tmi-road-permission') == 2 ? ' selected' : null; ?> value="2">2 Gün</option>
                     </select>
                 </label>
                 <label>
                     <p>Hava Değişimi/İstirahat Süresi</p>
-                    <input type="number" min="0" max="100" name="sick-leave">
+                    <input type="number" min="0" max="100" name="sick-leave" value="<?php echo post('sick-leave') ? post('sick-leave') : null; ?>" placeholder="0 Gün">
                 </label>
             </div>
             <div class="safak-wrapper">
                 <label>
                     <p>Firar/İzin Tecavüzü/Geç Katılış</p>
-                    <input type="number" min="0" max="100" name="escape">
+                    <input type="number" min="0" max="100" name="escape" value="<?php echo post('escape') ? post('escape') : null; ?>" placeholder="0 Gün">
                 </label>
                 <label>
                     <p>Alınan Ceza</p>
-                    <input type="number" min="0" max="100" name="penalty">
+                    <input type="number" min="0" max="100" name="penalty" value="<?php echo post('penalty') ? post('penalty') : null; ?>" placeholder="0 Gün (Ceza Almadım)">
                 </label>
                 <label>
-                    <p>Yol İzin Hakkı</p>
+                    <p>Erken Terhis</p>
                     <select name="early-leave">
-                        <option value="0">0 Gün (Yok)</option>
-                        <option value="1">1 Gün</option>
-                        <option value="2">2 Gün</option>
-                        <option value="3">3 Gün</option>
+                        <option <?php echo post('early-leave') == 0 ? ' selected' : null; ?> value="0">0 Gün (Yok)</option>
+                        <option <?php echo post('early-leave') == 1 ? ' selected' : null; ?> value="1">1 Gün</option>
+                        <option <?php echo post('early-leave') == 2 ? ' selected' : null; ?> value="2">2 Gün</option>
+                        <option <?php echo post('early-leave') == 3 ? ' selected' : null; ?> value="3">3 Gün</option>
                     </select>
                 </label>
             </div>
-            <div class="safak-wrapper">
+            <div class="safak-buttons">
                 <input type="hidden" name="submit" value="1">
                 <input type="submit" value="Hesapla">
+                <input type="button" value="Sıfırla" onclick="fun()">
             </div>
         </form>
-        <div class="result">
-            <div class="item">Askerlik Süresi:<?php ?></div>
-        </div>
+        <?php if(post('submit') && null == $errorMsg ): ?>
+            <div class="result">
+                <h3 class="safak-result">SONUÇLAR</h3>
+                <table class="table-fill">
+                    <tbody class="table-hover">
+                    <tr>
+                        <td class="text-left">Askerlik Süresi</td>
+                        <td class="text-left"><?php echo $militaryServiceTimeResult . ' Gün'; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">Kalan Kanuni İzin Hakkı</td>
+                        <td class="text-left"><?php echo $remainLeaveResult . ' Gün'; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">Resmi Terhis Tarihi</td>
+                        <td class="text-left"><?php echo $leaveFormatTimeResult . ' ' . date('l', strtotime($leaveTimeResult)) . ' Günü'; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">TMİ (Terhis Mahiyetinde İzin)</td>
+                        <td class="text-left"><?php echo $tmiFormatResult . ' ' . date('l', strtotime($tmiResult)) . ' Günü'; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">TMİ'ye kalan süre</td>
+                        <td class="text-left"><?php echo $remainTMIResult . ' Gün'; ?></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        <?php endif; ?>
     </div>
-
+    <script>
+        if ( window.history.replaceState ) {
+            window.history.replaceState( null, null, window.location.href );
+        }
+        const err = document.querySelector('.error-message');
+        if(err.textContent !== ''){
+            err.classList.add('active');
+            setTimeout(() => {
+                err.classList.remove('active');
+            }, 3000);
+        }
+        function fun(){
+            location.reload();
+        }
+    </script>
 </body>
 </html>
--- options.php
+++ options.php
@@ -1,45 +1,126 @@
 <?php
 
-function form_filter($get){
-    return is_array($get) ? array_map('form_filter', $get) : htmlspecialchars(trim($get));
-}
-$_GET = array_map('form_filter', $_GET);
-
-function get($name){
-    if(isset($_GET[$name]))
-        return $_GET[$name];
+function form_filter($post)
+{
+    return is_array($post) ? array_map('form_filter', $post) : htmlspecialchars(trim($post));
 }
 
-if(get('submit')){
-    $militaryServiceTime = get('military-service-time');
-    $shipDate = get('ship-date');
-    $permissionUsed = get('permission-used');
-    $roadPermission = get('road-permission');
-    $tmi = get('tmi-road-permission');
-    $sickLeave = get('sick-leave');
-    $escape = get('escape');
-    $penalty = get('penalty');
-    $earlyLeave = get('early-leave');
+$_POST = array_map('form_filter', $_POST);
 
-    function leave_time($time, $day, $permissionUsed, $escape, $penalty){
+function post($name)
+{
+    if ( isset($_POST[$name]) )
+        return $_POST[$name];
+}
+
+$errorMsg = '';
+
+if ( post('submit') && null == $errorMsg ) {
+    $militaryServiceTime = post('military-service-time');
+    $shipDate = post('ship-date');
+    $permissionUsed = post('permission-used');
+    $roadPermission = post('road-permission');
+    $tmi = post('tmi-road-permission');
+    $sickLeave = post('sick-leave');
+    $escape = post('escape');
+    $penalty = post('penalty');
+    $earlyLeave = post('early-leave');
+
+    function errors($mt, $pu, $mst, $sd)
+    {
+        $temp = (int)$pu;
+        $result = (int)$mt / 30;
+        if ( $result == 1 && $temp > 1 ) {
+            return 'Kullanılan izin alanı 1\'den fazla olamaz. Lütfen tekrar kontrol ediniz.';
+        } elseif ( $result == 6 && $temp > 6 ) {
+            return 'Kullanılan izin alanı 6\'dan fazla olamaz. Lütfen tekrar kontrol ediniz.';
+        } elseif ( $mst == 0 ) {
+            return 'Lütfen askerlik süresini giriniz.';
+        } elseif ( $sd == null ) {
+            return 'Lütfen sevk tarihini giriniz.';
+        } else {
+            return null;
+        }
+    }
+
+    $errorMsg = errors($militaryServiceTime, $permissionUsed, $militaryServiceTime, $shipDate);
+
+    function leave_time($time, $day, $permissionUsed, $escape, $penalty)
+    {
         $temp = explode('-', $time);
         $t = (($day / 30) == 6 || ($day / 30) == 12) ? 6 : 1;
-        $d = ((int)$permissionUsed - $t > 0 ) ? (int)$permissionUsed - $t : 0;
+        $d = ((int)$permissionUsed - $t > 0) ? (int)$permissionUsed - $t : 0;
         $m = ($temp[1] + ($day / 30));
-        $month = ($m / 12 > 1) ? ($m % 12) : $m;
+
+        if ( ($m / 12 > 1) ) {
+            $month = ($m % 12);
+            $temp[0]++;
+        } else {
+            $month = $m;
+        }
         $days = $d + (int)$escape + (int)$penalty;
-        echo 'days: ' . $days . '<br>';
-        return date('Y-m-d', strtotime("".$temp[0]."-".$month."-".$temp[2]." +".$days." day"));
+        return date('Y-m-d', strtotime("" . $temp[0] . "-" . $month . "-" . $temp[2] . " +" . $days . " day"));
     }
+
     $leaveTimeResult = leave_time($shipDate, $militaryServiceTime, $sickLeave, $escape, $penalty);
 
-    function military_service_time($leaveTimeResult){
-        $datediff = (strtotime($leaveTimeResult) - time() );
+    function military_service_time($leaveTimeResult)
+    {
+        $datediff = (strtotime($leaveTimeResult) - time());
         return round($datediff / (60 * 60 * 24));
     }
+
     $militaryServiceTimeResult = military_service_time($leaveTimeResult);
-    echo $militaryServiceTimeResult . ' x <br>';
-    echo $leaveTimeResult . ' a <br>';
+
+    function remainLeave($mt, $pu)
+    {
+        $temp = (int)$pu;
+        $result = (int)$mt / 30;
+        if ( $result == 1 && $temp <= 1 ) {
+            return 1 - $temp;
+        } elseif ( $result == 6 && $temp <= 6 ) {
+            return 6 - $pu;
+        } elseif ( $result == 12 && $temp <= 24 ) {
+            return 24 - $temp;
+        }
+    }
+
+    $remainLeaveResult = remainLeave($militaryServiceTime, $permissionUsed);
+
+    function TMI($leaveTimeResult, $tmi, $earlyLeave, $remainLeaveResult)
+    {
+        $dayDiff = (int)$earlyLeave + (int)$tmi + (int)$remainLeaveResult;
+        return date('Y-m-d', strtotime("$leaveTimeResult -" . $dayDiff . " day"));
+    }
+
+    $tmiResult = TMI($leaveTimeResult, $tmi, $earlyLeave, $remainLeaveResult);
+
+    function remainTMI($tmiResult)
+    {
+        $datediff = (strtotime($tmiResult) - time());
+        return round($datediff / (60 * 60 * 24));
+    }
+
+    $remainTMIResult = remainTMI($tmiResult);
+
+    function dateFormat($format)
+    {
+        $temp = explode('-', $format);
+        return implode('/', array_reverse($temp));
+    }
+
+    $tmiFormatResult = dateFormat($tmiResult);
+    $leaveFormatTimeResult = dateFormat($leaveTimeResult);
 }
 
-print_r($_GET);
+
+
+
+
+
+
+
+
+
+
+
